121cd1294cd23a6ade4c19b590c268d4af4d76b6
heysushil <sushil.chaudhary111@gmail.com>
2020-04-07 14:22:33 +0530

    metplotlib commit

--- numpy/array_ploat.py
+++ numpy/array_ploat.py
@@ -2,6 +2,6 @@ from numpy import random
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-sns.distplot(random.uniform(size=1000))
+sns.distplot(random.uniform(size=1000),hist=False)
 
 plt.show()
