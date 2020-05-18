b20fab511e2143619d1a16006cb6c65bfe15fa25
heysushil <sushil.chaudhary111@gmail.com>
2020-05-18 12:52:08 +0530

    list tuple and set class

--- basics/array.py
+++ basics/array.py
@@ -38,9 +38,50 @@ newlist.append('Tripati')
 print(newlist)
 
 del newlist
-print(newlist)
+# print(newlist)
+
+# ankit personal detail
+ankit = ['ankit','python','delhi']
+
+# add mobile number using append
+ankit.append('8798798779')
+
+# append hamesa value ko list ke last me add karta hai
+ankit.append('delhi')
+
+# pop hamesa list me last value remove karta hai
+ankit.pop(3)
+print('\nAnkit = ',ankit)
+
+# single value in tupel
+
+singltuple = ('hello python',)
+listval = ['hell']
+print(type(listval))
+print(type(singltuple))
 
 
 '''
+# Set
 
+    1. Set unorder form me hota hai. isko {} se denote karte hai
+    2. set union aur intersection ke liye use hota hai
+    3. 
+'''
+
+
+'''
+# home work
+    1. change tuple values. useing casting
+    2. usring for loop to print your perstionl information line by line
+    3. for loop ka use karke ek tuple me pade tumhare pernal details ko show karna hai iske baad tuple me abni information ko modify kar ke dubara for loop me updated values ko show karan hai.
+
+# Set Home work
+
+    1. ek set banao aur usko print karo 4 bar aur notic karo ki har ek print output me value ka index change hoga
+    2. ek set bana ke uske kisi ek index possion ki value ko print karke dekho ki kya output mil raha hai.
+    3. ek set banake usko for loop me chala ke outputs print karo.
+    4. add multiple values in set uske baad for loop ka use karke print karo.
+    5. ek set banao aur usem 4 new values ko ek baar me add karo aur for loop ka use karke print karo. iske baad me 3 value ko remove karo agar wo value set me nahi hai to errro show nahi hona chaiye uske liye kaonsa method user karna hai set ka wo pata karo.
+    6. set ke sare method ko ek baar use kar kar ke dekh lo.
 '''
